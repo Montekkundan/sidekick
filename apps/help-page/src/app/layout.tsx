3ac3280e7e48092071165666bbf85481b242994c
Montek <montekkundan@gmail.com>
2026-01-13 14:19:44 -0500

    base template for all cookbooks

---
+++ apps/help-page/src/app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import "./styles.css";
+import { DesignSystemProvider } from "@repo/design-system";
+import { fonts } from "@repo/design-system/lib/fonts";
+import { cn } from "@repo/design-system/lib/utils";
+
+export const metadata: Metadata = {
+  title: "shadturbo — Turborepo starter with shadcn/ui",
+  description:
+    "Minimal starter repo that includes a Next.js app and a shadcn design system.",
+  openGraph: {
+    title: "shadturbo — Turborepo starter with shadcn/ui",
+    description:
+      "Minimal starter repo that includes a Next.js app and a shadcn design system.",
+    url: "https://shadturbo.vercel.app",
+    siteName: "shadturbo",
+    images: [
+      {
+        url: "/api/og",
+        width: 1200,
+        height: 630,
+        alt: "shadturbo — Turborepo starter with shadcn/ui",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "shadturbo — Turborepo starter with shadcn/ui",
+    description:
+      "Minimal starter repo that includes a Next.js app and a shadcn design system.",
+    creator: "@montekkundan",
+    images: ["/api/og"],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      className={cn(fonts, "scroll-smooth")}
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body>
+        <DesignSystemProvider>{children}</DesignSystemProvider>
+      </body>
+    </html>
+  );
+}
